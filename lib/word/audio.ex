861f425a4c5763130ff8eeceb000f1617461f5cd
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-21 22:50:47 -0400

    add audio query

---
+++ lib/word/audio.ex
@@ -0,0 +1,28 @@
+defmodule Word.Audio do
+  alias HTTPoison
+
+  defp format_url(word, api_key) do
+    "http://api.wordnik.com/v4/word.json/#{word}/audio?api_key=#{api_key}"
+  end
+
+  defp format_param(limit: limit), do: "limit=#{limit}"
+  defp format_param(:use_canonical), do: "useCanonical=true"
+
+  defp format_params(url, [head | tail]) do
+    param = format_param(head)
+    format_params(url <> "&" <> param, tail)
+  end
+
+  defp format_params(url, []), do: url
+
+  def get_audio(word, api_key, params \\ []) do
+    resp = format_url(word, api_key)
+    |> format_params(params)
+    |> HTTPoison.get([], [follow_redirect: true])
+
+    case resp do
+      {:ok, content} -> Jason.decode!(content.body)
+      {:error, _} -> resp
+    end
+  end
+end
