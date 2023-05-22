d19f245e047a3386c5c5359d7ec3ac327927ae70
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-21 22:53:29 -0400

    add scrabble score

---
+++ lib/word/scrabble_score.ex
@@ -0,0 +1,15 @@
+defmodule Word.ScrabbleScore do
+  defp format_url(word, api_key) do
+    "http://api.wordnik.com/v4/word.json/#{word}/scrabbleScore?api_key=#{api_key}"
+  end
+
+  def get_scrabble_score(word, api_key) do
+    resp = format_url(word, api_key)
+    |> HTTPoison.get([], [follow_redirect: true])
+
+    case resp do
+      {:ok, content} -> Jason.decode!(content.body)
+      {:error, _} -> resp
+    end
+  end
+end
