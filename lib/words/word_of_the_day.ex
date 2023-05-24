df0eef4b4c7ccd0a827bf7b5b7ae1c8d7b0c4594
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 18:05:25 -0400

    refactor word of the day query and add validation

--- lib/words/word_of_the_day.ex
+++ lib/words/word_of_the_day.ex
@@ -1,25 +1,46 @@
 defmodule Words.WordOfTheDay do
+  @valid_params [
+    :date
+  ]
+
+  @fn_name "get_word_of_the_day"
+  @invalid_date_error_msg "Error: please provide a valid date in 'yyyy-MM-dd' format"
+
   defp format_url(api_key) do
     "http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=#{api_key}"
   end
 
-  defp format_param({:date, date}), do: "date=#{date}"
+  defp validate_date_format(date) do
+    yyyy_mm_dd = ~r/^\d{4}-((0\d)|(10)|(11)|(12))-((30)|(31)|([0-2]\d))$/
 
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
+    if String.match?(date, yyyy_mm_dd) do
+      invalid_feb_dates = ~r/^\d{4}-02-((30)|(31))$/
+
+      if String.match?(date, invalid_feb_dates) do
+        {:error, @invalid_date_error_msg}
+      else
+        {:ok, "valid date format"}
+      end
+    else
+      {:error, @invalid_date_error_msg}
+    end
   end
 
-  defp format_params(url, []), do: url
+  def get_word_of_the_day(api_key, params \\ [])
 
-  def get_related_words(api_key, params \\ []) do
-    resp = format_url(api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
+  def get_word_of_the_day(api_key, [{:date, date}]) do
+    {status, msg} = validate_date_format(date)
 
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
+    if status == :error do
+      {status, msg}
+    else
+      format_url(api_key)
+      |> Formatter.Params.validate_and_fetch_query([{:date, date}], @valid_params, @fn_name)
     end
+  end
+
+  def get_word_of_the_day(api_key, params) do
+    format_url(api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
