68f02af700bb2af954bb80eaef9180f2bcf8d0c8
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 01:57:26 -0400

    refactor pronunciations query

--- lib/word/pronunciations.ex
+++ lib/word/pronunciations.ex
@@ -1,47 +1,19 @@
 defmodule Word.Pronunciations do
-  @dictonaries [
-    "all",
-    "ahd-5",
-    "century",
-    "wiktionary",
-    "webster",
-    "wordnet"
+  @valid_params [
+    :limit,
+    :type_format,
+    :source_dict,
+    :use_canonical
   ]
 
-  @type_format [
-    "ahd-5",
-    "arpabet",
-    "gcide-diacritical",
-    "IPA"
-  ]
-
-  defguardp is_valid_dict(source_dict) when source_dict in @dictonaries
-  defguardp is_valid_type_format(format) when format in @type_format
+  @fn_name "get_pronunciations"
 
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/definitions?api_key=#{api_key}"
   end
 
-  defp format_param({:limit, limit}) when is_integer(limit), do: "limit=#{limit}"
-  defp format_param({:type_format, format}) when is_valid_type_format(format), do: "typeFormat=#{format}"
-  defp format_param({:source_dict, source_dict}) when is_valid_dict(source_dict), do: "sourceDictionary=#{source_dict}"
-  defp format_param(:use_canonical), do: "useCanonical=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_pronunciations(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
