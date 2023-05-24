1ca53fe10f0086efc29c92188c9641b1fea30782
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 02:15:44 -0400

    refactor top example query

--- lib/word/top_example.ex
+++ lib/word/top_example.ex
@@ -1,25 +1,16 @@
 defmodule Word.TopExample do
+  @valid_params [
+    :use_canonical
+  ]
+
+  @fn_name "get_top_example"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/topExample?api_key=#{api_key}"
   end
 
-  defp format_param(:use_canonical), do: "useCanonical=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_top_example(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
