6047177b81bf5030ae64b482cdbeba07cb236518
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 00:42:29 -0400

    refactor get_audio

--- lib/word/audio.ex
+++ lib/word/audio.ex
@@ -1,26 +1,17 @@
 defmodule Word.Audio do
+  @valid_params [
+    :limit,
+    :use_canonical
+  ]
+
+  @fn_name "get_audio"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/audio?api_key=#{api_key}"
   end
 
-  defp format_param({:limit, limit}), do: "limit=#{limit}"
-  defp format_param(:use_canonical), do: "useCanonical=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_audio(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
