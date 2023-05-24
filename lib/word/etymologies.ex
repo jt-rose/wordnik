a8d2596f3ee5c52e05c52f774da3363b69123ab2
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 01:06:46 -0400

    refactor etymologies query

--- lib/word/etymologies.ex
+++ lib/word/etymologies.ex
@@ -1,25 +1,34 @@
 defmodule Word.Etymologies do
+  @valid_params [
+    :use_canonical
+  ]
+
+  @fn_name "get_etymologies"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/etymologies?api_key=#{api_key}"
   end
 
-  defp format_param(:use_canonical), do: "useCanonical=true"
+  # defp format_param(:use_canonical), do: "useCanonical=true"
 
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
+  # defp format_params(url, [head | tail]) do
+  #   param = format_param(head)
+  #   format_params(url <> "&" <> param, tail)
+  # end
 
-  defp format_params(url, []), do: url
+  # defp format_params(url, []), do: url
 
   def get_etymologies(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
 
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    # resp = format_url(word, api_key)
+    # |> format_params(params)
+    # |> HTTPoison.get([], [follow_redirect: true])
+
+    # case resp do
+    #   {:ok, content} -> {:ok, Jason.decode!(content.body)}
+    #   {:error, _} -> resp
+    # end
   end
 end
