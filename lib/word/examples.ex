a66c38aaf144bb79f3b1228d4fe9b2042d2ad81b
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 01:15:20 -0400

    refactor examples query

--- lib/word/examples.ex
+++ lib/word/examples.ex
@@ -1,28 +1,19 @@
 defmodule Word.Examples do
+  @valid_params [
+    :limit,
+    :skip,
+    :use_canonical,
+    :include_duplicates
+  ]
+
+  @fn_name "get_examples"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/examples?api_key=#{api_key}"
   end
 
-  defp format_param({:limit, limit}) when is_integer(limit), do: "limit=#{limit}"
-  defp format_param({:skip, skip}) when is_integer(skip), do: "skip=#{skip}"
-  defp format_param(:use_canonical), do: "useCanonical=true"
-  defp format_param(:include_duplicates), do: "includeDuplicates=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_examples(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
