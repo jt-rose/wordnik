4eb94a833e34daf1247f88e1c84ececb30a30408
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 01:26:49 -0400

    refactor hyphenation query

--- lib/word/hyphenation.ex
+++ lib/word/hyphenation.ex
@@ -1,38 +1,18 @@
 defmodule Word.Hyphenation do
-  @dictonaries [
-    "all",
-    "ahd-5",
-    "century",
-    "wiktionary",
-    "webster",
-    "wordnet"
+  @valid_params [
+    :limit,
+    :source_dict,
+    :use_canonical
   ]
 
-  defguardp is_valid_dict(source_dict) when source_dict in @dictonaries
+  @fn_name "get_hyphenation"
 
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/hyphenation?api_key=#{api_key}"
   end
 
-  defp format_param({:limit, limit}), do: "limit=#{limit}"
-  defp format_param({:source_dict, source_dict}) when is_valid_dict(source_dict), do: "sourceDictionary=#{source_dict}"
-  defp format_param(:use_canonical), do: "useCanonical=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_hyphenation(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
