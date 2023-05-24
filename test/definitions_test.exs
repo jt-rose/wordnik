949f664f6534f2814e5a267bd849ec49ea071fe6
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 00:35:02 -0400

    update mix formatting

--- test/definitions_test.exs
+++ test/definitions_test.exs
@@ -3,21 +3,27 @@ defmodule DefinitionsTest do
 
   @test_word "elixir"
   @definition "A sweetened aromatic solution of alcohol and water, serving as a vehicle for medicine."
-  @api_key System.get_env "WORDNIK_API_KEY"
+  @api_key System.get_env("WORDNIK_API_KEY")
 
   test "get definitions" do
     {status, resp} = Word.Definitions.get_definitions(@test_word, @api_key)
 
-    found = Enum.find(resp, fn(item) -> item["text"] == @definition end)
+    found = Enum.find(resp, fn item -> item["text"] == @definition end)
     assert status == :ok
     assert found != nil
   end
 
   test "get definitions with params" do
-    {status, resp} = Word.Definitions.get_definitions(@test_word, @api_key, [:use_canonical, :include_related, :include_tags, part_of_speech: "noun", source_dict: "ahd-5"])
-    IO.inspect resp
+    {status, resp} =
+      Word.Definitions.get_definitions(@test_word, @api_key, [
+        :use_canonical,
+        :include_related,
+        :include_tags,
+        part_of_speech: "noun",
+        source_dict: "ahd-5"
+      ])
 
-    found = Enum.find(resp, fn(item) -> item["text"] == @definition end)
+    found = Enum.find(resp, fn item -> item["text"] == @definition end)
     assert status == :ok
     assert found != nil
   end
@@ -27,5 +33,4 @@ defmodule DefinitionsTest do
     assert status == :error
     assert msg == "'whoops' not a valid parameter for the 'get_definitions' function"
   end
-
 end
