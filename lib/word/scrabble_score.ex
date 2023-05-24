410b5d383f23a661cfa4aae6523f1a6737dee560
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 02:10:02 -0400

    refactor scrabble score query

--- lib/word/scrabble_score.ex
+++ lib/word/scrabble_score.ex
@@ -1,15 +1,12 @@
 defmodule Word.ScrabbleScore do
+  @fn_name "get_scrabble_score"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/scrabbleScore?api_key=#{api_key}"
   end
 
   def get_scrabble_score(word, api_key) do
-    resp = format_url(word, api_key)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query([], [], @fn_name)
   end
 end
