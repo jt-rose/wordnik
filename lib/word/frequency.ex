d132869973b993fd51dc6d3860fd3971015d5715
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-05-24 01:20:48 -0400

    refactor frequency query

--- lib/word/frequency.ex
+++ lib/word/frequency.ex
@@ -1,27 +1,18 @@
 defmodule Word.Frequency do
+  @valid_params [
+    :start_year,
+    :end_year,
+    :use_canonical
+  ]
+
+  @fn_name "get_frequency"
+
   defp format_url(word, api_key) do
     "http://api.wordnik.com/v4/word.json/#{word}/frequency?api_key=#{api_key}"
   end
 
-  defp format_param({:start_year, start_year}) when is_integer(start_year), do: "startYear=#{start_year}"
-  defp format_param({:end_year, end_year}) when is_integer(end_year), do: "endYear=#{end_year}"
-  defp format_param(:use_canonical), do: "useCanonical=true"
-
-  defp format_params(url, [head | tail]) do
-    param = format_param(head)
-    format_params(url <> "&" <> param, tail)
-  end
-
-  defp format_params(url, []), do: url
-
   def get_frequency(word, api_key, params \\ []) do
-    resp = format_url(word, api_key)
-    |> format_params(params)
-    |> HTTPoison.get([], [follow_redirect: true])
-
-    case resp do
-      {:ok, content} -> {:ok, Jason.decode!(content.body)}
-      {:error, _} -> resp
-    end
+    format_url(word, api_key)
+    |> Formatter.Params.validate_and_fetch_query(params, @valid_params, @fn_name)
   end
 end
